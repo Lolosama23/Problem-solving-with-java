1a86aba75a04f3c2a3c414d7df31d5e7304f408e
loaio <loaiosama20@gmai.com>
2023-08-25 10:29:40 +0300

    Data Types

---
+++ src/main/java/DataTypes.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author loaio
+ */
+import java.util.Scanner;
+public class DataTypes {
+      public static void main(String[] args){
+          Scanner input = new Scanner (System.in);
+           int x = input.nextInt();
+       long  y = input.nextLong();
+       char z = input.next().charAt(0);
+       float f = input.nextFloat();
+       double d = input.nextDouble();
+       
+         System.out.println(y);
+         System.out.println(y);
+         System.out.println(z);
+         System.out.println(f);
+         System.out.println(d);
+      }
+              
+    
+}
