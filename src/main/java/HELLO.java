0b09800e4dd5c9fe91613768ccf0a13078f583ce
loaio <loaiosama20@gmai.com>
2023-08-25 10:13:24 +0300

    Say Hello

---
+++ src/main/java/HELLO.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author loaio
+ */
+import java.util.Scanner;
+public class HELLO {
+    public static void main(String[] args) {
+       Scanner input = new Scanner (System.in);
+       String S = input.next();
+        System.out.println("Hello, "+S);
+    }
+}
