b5c28da8f86f33f60bc64525e1ebeb78da098d56
loaio <loaiosama20@gmai.com>
2023-08-26 19:55:00 +0300

    Summation Numbers from 1 to N

---
+++ src/main/java/SummationNums.java
@@ -0,0 +1,20 @@
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author loaio
+ */
+import java.util.Scanner;
+public class SummationNums {
+                   public static void main(String[] args){
+           Scanner input = new Scanner (System.in);
+           long n = input.nextLong();
+                       System.out.println(n*(n+1)/2);
+                   }
+}
