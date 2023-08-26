1a541eb772e4eef3953353deb1da92d457c98e39
loaio <loaiosama20@gmai.com>
2023-08-26 19:43:18 +0300

    Digits Summation

---
+++ src/main/java/DigitsSummation.java
@@ -0,0 +1,21 @@
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author loaio
+ */
+import java.util.Scanner;
+public class DigitsSummation {
+               public static void main(String[] args){
+           Scanner input = new Scanner (System.in);
+           long x = input.nextLong();
+            long y = input.nextLong();
+                   System.out.println(x%10+y%10);
+               }
+}
