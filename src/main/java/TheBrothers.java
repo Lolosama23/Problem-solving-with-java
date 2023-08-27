fd4a202ee0fc3887d3c55158bbcabb3abd816965
loaio <loaiosama20@gmai.com>
2023-08-27 09:41:24 +0300

    Brothers

---
+++ src/main/java/TheBrothers.java
@@ -0,0 +1,27 @@
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author loaio
+ */
+import java.util.Scanner;
+public class TheBrothers {
+                     public static void main(String[] args){
+           Scanner input = new Scanner (System.in);
+           String x1 = input.next();
+           String x2 = input.next();
+             String y1 = input.next();
+               String y2 = input.next();
+               if(x2.equals(y2))
+                             System.out.println("ARE Brothers");
+               else
+                             System.out.println("NOT");
+           
+                     }
+}
