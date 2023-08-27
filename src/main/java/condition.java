934940cf5b29b9a0971f1e8481bfc3988f9e2b56
loaio <loaiosama20@gmai.com>
2023-08-27 08:09:44 +0300

    Welcome for you with Conditions

---
+++ src/main/java/condition.java
@@ -0,0 +1,27 @@
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author loaio
+ */
+   import java.util.Scanner;
+    public class condition {
+                       public static void main(String[] args){
+           Scanner input = new Scanner (System.in);
+           int x = input.nextInt();
+           int y = input.nextInt();
+           if(x>=y)
+           
+               System.out.println("Yes");
+           else
+                           System.out.println("No");
+           
+          
+                       }
+    }
